fbf53920a54bf52dabbeeefd15cd0a59db7b73bb
Ignacio Ballesteros <igbprogramming@gmail.com>
2024-12-12 21:05:53 -0500

    finished the implementation of the queue using linked lists

--- Queues/linked.cpp
+++ Queues/linked.cpp
@@ -5,20 +5,65 @@ struct Node {
     Node* next;
 };
 
-Node createNode() {}
+Node* createNode() {
+    Node* temp = new Node();
 
-void addNode(Node** head, Node* temp) {}
+    std::cout << "Enter data (int): ";
+    std::cin >> temp->data;
+    temp->next = nullptr;
 
-void displayQueue(Node* head) {}
+    return temp;
+}
 
-void removeNode(Node** head) {}
+void addNode(Node** head, Node* temp) {
+    Node* temp1 = *head;
 
-bool isEmpty(Node** head) {}
+    if (*head == nullptr) {
+        *head = temp;
+    }
+    else {
+        while (temp1->next != nullptr) {
+            temp1 = temp1->next;
+        }
+        temp1->next = temp;
+    }
+}
 
-void topElement(Node** head) {}
+void displayQueue(Node** head) {
+    Node* temp = *head;
+    int position = 1;
+
+    while (temp != nullptr) {
+        std::cout << "Position: " << position << "   ||   Data: " << temp->data << "\n";
+        position++;
+        temp = temp->next;
+    }
+    std::cout << "\n";
+}
+
+void removeNode(Node** head) {
+    Node* temp = *head;
+    *head = temp->next;
+    delete(temp);
+}
+
+bool isEmpty(Node** head) {
+    if (*head == nullptr) {
+        return true;
+    }
+    return false;
+}
+
+void topElement(Node** head) {
+    Node* temp = *head;
+
+    std::cout << "The top of the queue is: " << temp->data << "\n\n";
+}
 
 int main() {
 
+    Node* head = nullptr;
+    Node* temp = nullptr;
     int option = 0;
 
     std::cout << "Welcome to the queue program (linked lists) - choose an option:\n";
@@ -32,5 +77,35 @@ int main() {
         std::cout << "Option 6+ - Exit\n";
 
         std::cin >> option;
+
+        if (option == 1) {
+            int num = 0;
+            std::cout << "How many numbers do you want to add to the queue?\n";
+            std::cin >> num;
+
+            for (int i = 0; i < num; i++) {
+                temp = createNode();
+                addNode(&head, temp);
+            }
+            std::cout << "\n";
+        }
+        else if (option == 2) {
+            displayQueue(&head);
+        }
+        else if (option == 3) {
+            removeNode(&head);
+        }
+        else if (option == 4) {
+            if (isEmpty(&head)) {
+                std::cout << "Queue is empty\n\n";
+            }
+            else {
+                std::cout << "Queue is not empty\n\n";
+            }
+        }
+        else if (option == 5) {
+            topElement(&head);
+        }
+
     }
 }
